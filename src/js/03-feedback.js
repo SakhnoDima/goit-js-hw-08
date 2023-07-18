764bde5b60a17ac88891202e42d3c7374017df57
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-07-18 18:44:35 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,11 @@
+const refs = {
+formEl : document.querySelector(".feedback-form"),
+}
 
+refs.formEl.addEventListener("submit", onFormSubmit )
+
+function onFormSubmit (event){
+
+console.log();
+
+}
