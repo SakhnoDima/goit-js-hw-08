1c080a53b42fc1e99473c98ac19ccd5594d459c7
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-07-18 15:45:50 +0000

    Deploying to gh-pages from @ SakhnoDima/goit-js-hw-08@764bde5b60a17ac88891202e42d3c7374017df57 🚀

---
+++ 03-feedback.11de1ef8.js
@@ -0,0 +1,2 @@
+({formEl:document.querySelector(".feedback-form")}).formEl.addEventListener("submit",(function(e){console.log()}));
+//# sourceMappingURL=03-feedback.11de1ef8.js.map
---
+++ 03-feedback.7ee3b465.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form").addEventListener("submit",(function(e){console.log()}));
+//# sourceMappingURL=03-feedback.7ee3b465.js.map
