59e230c7edd90c44668ff967afdb405d644021e7
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-07-19 23:09:29 +0300

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,7 +6,7 @@ const iframe = document.querySelector("#vimeo-player");
 
     const player = new Player(iframe);
 
-    player.on('timeupdate',  throttle(saveTime, 1));
+    player.on('timeupdate',  throttle(saveTime, 1000));
 
     function saveTime (event){
         localStorage.setItem("videoplayer-current-time", event.seconds);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,26 +1,27 @@
 import throttle from 'lodash.throttle';
+
 const STORAGE_KAY = ".feedback-form"
 const messageFromForm = {};
-
+let userData = JSON.parse(localStorage.getItem(STORAGE_KAY))  || {};
 const refs = {
 formEl : document.querySelector(STORAGE_KAY),
 }
+
 
 refs.formEl.addEventListener("submit", onFormSubmit )
 refs.formEl.addEventListener("input",   throttle(onFormTextInput, 500));
 
 addMessageFromLocalStorage()
 
+// ?=======================================================
 function onFormSubmit (event){
 event.preventDefault();
 
-const messageFromForm = JSON.parse(localStorage.getItem(STORAGE_KAY));
-console.log(messageFromForm);
-
+console.log(userData);
 event.currentTarget.reset();
 localStorage.removeItem(STORAGE_KAY);
 }
- 
+//  ?========================================================
 function onFormTextInput (event){
 messageFromForm[event.target.name] = event.target.value;
 
@@ -28,17 +29,14 @@ const messageForSave = JSON.stringify(messageFromForm)
 localStorage.setItem(STORAGE_KAY, messageForSave)
 
 }
-
+// ?==========================================================
 function addMessageFromLocalStorage(){
-const messageFromFormNew = JSON.parse(localStorage.getItem(STORAGE_KAY));
 
-   
+const {email, message} = userData;
 
-const {email, message} = messageFromFormNew;
-
-if (email && message) {
+//if (email || message) {
     
-refs.formEl.elements.email.value = email;
-refs.formEl.elements.message.value = message;
-}
+refs.formEl.elements.email.value = email ||  "";
+refs.formEl.elements.message.value = message ||  "";
+//}
 }
