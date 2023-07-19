67ad150e207abf2369dc8eeedff00824e8777f5a
SakhnoDima <134371251+SakhnoDima@users.noreply.github.com>
2023-07-19 09:13:51 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,41 +3,39 @@ const STORAGE_KAY = ".feedback-form"
 const messageFromForm = {};
 
 const refs = {
-formEl : document.querySelector(".feedback-form"),
+formEl : document.querySelector(STORAGE_KAY),
 }
 
 refs.formEl.addEventListener("submit", onFormSubmit )
 refs.formEl.addEventListener("input",   throttle(onFormTextInput, 500));
 
-addMessageFromLocalStorage()
+// addMessageFromLocalStorage()
 
 function onFormSubmit (event){
 event.preventDefault();
 
-const messageFromForm = JSON.parse(localStorage.getItem("feedback-form-state"));
+const messageFromForm = JSON.parse(localStorage.getItem(STORAGE_KAY));
 console.log(messageFromForm);
 
 event.currentTarget.reset();
-localStorage.removeItem("feedback-form-state");
+localStorage.removeItem(STORAGE_KAY);
 }
  
 function onFormTextInput (event){
 messageFromForm[event.target.name] = event.target.value;
 
 const messageForSave = JSON.stringify(messageFromForm)
-localStorage.setItem("feedback-form-state", messageForSave)
+localStorage.setItem(STORAGE_KAY, messageForSave)
 
 }
 
 function addMessageFromLocalStorage(){
-const messageFromFormNew = JSON.parse(localStorage.getItem("feedback-form-state"));
+const messageFromFormNew = JSON.parse(localStorage.getItem(STORAGE_KAY));
 
     console.log(messageFromFormNew);
 
 const {email, message} = messageFromFormNew;
-
 if (email && message) {
- 
 refs.formEl.elements.email.value = email;
 refs.formEl.elements.message.value = message;
 }
